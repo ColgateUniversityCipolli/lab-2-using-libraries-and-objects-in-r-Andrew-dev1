ce476b5c6d05fa0627bfbb31b8b0901ecd6cacc6
Andrew-dev1 <andrli20140@gmail.com>
2025-02-03 23:11:00 -0500

    finished the lab

--- lab2.R
+++ lab2.R
@@ -1,61 +1,53 @@
-main.dir <- "music/"
+main.dir <- "MUSIC/"
 vec.musics <- list.dirs(main.dir)
 
-# subsetting all the album files by 
+# subsetting all the album files by taking out all of the subdirectories
 directs <- str_count(vec.musics, pattern = "/")
 subdirects.indices <- which(directs == 3)
-subdirects <- rep(x = NA, times = length(subdirects.indices))
-counter <- 1
-for(i in subdirects.indices){
-  subdirects[counter] <- vec.musics[i]
-  counter <- counter + 1
-}
-
-all.files <- list.files(subdirects)
-file.indices <- which(str_count(all.files, pattern = ".wav") == 1)
-wav.files <- rep(x = NA, times = length(file.indices))
-counter <- 1
-for(file.num in file.indices){
-  wav.files[counter] <- all.files[file.num]
-  counter <- counter + 1
-}
-
-code.to.process <- rep(x = NA, times = length(wav.files))
-counter <- 1
-for(file in wav.files){
-  x <- paste(file)
-  x <- str_sub(x,1, length(x)-6)
-  x <- str_split(x, "-")
-  code.to.process[counter] <- x[2] + x[3]
-  counter <- counter +1
-} 
+subdirects <- vec.musics[subdirects.indices]
 
 
 code.to.process <- c()
+# iterate through all subdirectories of the artists and take out only the wave files
 for(i in 1:length(subdirects)){
   current <- list.files(subdirects[i])
   file.indices <- which(str_count(current, pattern = ".wav") == 1)
-  wav.files <- rep(x = NA, times = length(file.indices))
-  shortened <- str_split(subdirects[i], "/")
-  album.name <- shortened[[1]][4]
+  wav.files <- current[file.indices]
+  shortened <- str_split(subdirects[i], "/", simplify = T)
+  album.name <- shortened[4]
+  print(album.name)
   
-  for(file.in.sub in current){
+  for(file.in.sub in wav.files){
     x <- paste(file.in.sub)
     x <- str_sub(x,1, length(x)-6)
-    x <- str_split(x, "-")
-    final.parts <- x[[1]]
-    # print(str_c(final.parts[2],album.name, final.parts[3], sep = "-"))
+    final.parts <- str_split(x, "-", simplify = T)
     sentence <- str_c(final.parts[2],album.name, final.parts[3], sep = "-")
     sentence <- paste("streaming_extractor_music.exe ", file.in.sub, str_c(sentence, ".json"))
     
     code.to.process <- append(code.to.process,sentence)
   }
 }
+#writes in all of the 
 fileConn<-file("batfile.txt")
 writeLines(code.to.process, fileConn)
 close(fileConn)
 
-name.file <- "The Front Bottoms-Talon Of The Hawk-Au Revoir (Adios).json"
-parts.of.name <- str_split(name.file, "-")[[1]]
-object <- fromJSON(name.file)
 
+#Task 2
+  #splitting the items from the file name  
+name.files <- list.files()
+current.file <- name.files[11]
+parts.of.name <- str_split(current.file, "-", simplify = T)
+artist <- parts.of.name[1]
+album <- parts.of.name[2]
+track <- parts.of.name[3]
+
+  #loading in the json file and extracting each item 
+object <- fromJSON(current.file)
+average.loudness <- object$lowlevel$average_loudness
+mean.spectral.energy <- object$lowlevel$spectral_energy$mean
+danceability <- object$rhythm$danceability
+bpm <- object$rhythm$bpm
+key_key <- object$tonal$key_key
+key_scale <- object$tonal$key_scale
+length <- object$metadata$audio_properties$length
